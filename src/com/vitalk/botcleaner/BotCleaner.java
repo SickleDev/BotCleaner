bbb40dda45850d5b19aa4027957c6934c3a0940f
sickledev <sickledev@gmail.com>
2021-03-01 11:01:58 +0100

    Setting up repo

---
+++ src/com/vitalk/botcleaner/BotCleaner.java
@@ -0,0 +1,10 @@
+package com.vitalk.botcleaner;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BotCleaner extends Plugin {
+    @Override
+    public void onEnable(){
+        getLogger().info("Coming Soon !");
+    }
+}
